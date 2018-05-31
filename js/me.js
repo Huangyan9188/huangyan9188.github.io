35070ba240de03cf7480fd1d0da4b7d9b4683842
huangyan9188 <huangyan9188@163.com>
2018-05-31 15:18:49 +0800

    add changes

---
+++ js/me.js
@@ -0,0 +1,32 @@
+profile={
+    greeting:"Hey,I am H-Young",
+    message:"developer"
+}
+timeline=[
+    {
+        name:"Backbone Framework",
+        describe:"Is the Framework for web based on components reusing style; Tools for building components, config components, and reusing components; Tools for github deployment and sofeware release; Tools for Testing"
+    },
+    {
+        name:"Monica",
+        describe:"The Realtime Data Computing Framework, which is build on golang, c and lua; Streaming Modules can be configed to define the processing routine; SDK for Client; Scalable Architecture and library for detail needs;"
+    },{
+        name:"ADU",
+        describe:"The Auth Service for dealing with the relationship among devices, developer, project and users, based on OAuth2.0 "
+    },{
+        name:"NSQBlend",
+        describe:"The High Perfermence and full ability version for NSQ Messaging Bus."
+    },{
+        name:"NSQElastic",
+        describe:"The Distribute version of NSQ Data Bus."
+    },{
+        name:"LivingMap",
+        describe:"The LivingMap based on 3d webgl that can see every people's realtime position on the real map, and check any people's tracks before."
+    },{
+        name:"DeepV Realtime Client",
+        describe:"The Web Client that can recongnize all the cars' type, model, color, plate and inner things like safe belt, tissue box and so on "
+    },{
+        name:"Deepglint Website",
+        describe:"deepglint.com"
+    }
+]
