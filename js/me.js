ed2e3bbd9074fec5621801365a5b04f51f76ce42
huangyan9188 <huangyan9188@163.com>
2018-06-02 08:58:54 +0800

    add changs

--- js/me.js
+++ js/me.js
@@ -1,15 +1,29 @@
 profile={
     greeting:"Hey,I am H-Young",
-    message:"developer"
+    message:"你好，世界！"
 }
 timeline=[
     {
-        name:"Backbone Framework",
-        describe:"Is the Framework for web based on components reusing style; Tools for building components, config components, and reusing components; Tools for github deployment and sofeware release; Tools for Testing"
+        name:"Trillion Pairs Face Competition,Chief Support[Side Project],格灵深瞳,[2018.1.1-NOW]",
+        describe:"一个全新的人脸识别竞赛",
+        more:"http://trillionpairs.deepglint.com/overview"
     },
     {
-        name:"Monica",
-        describe:"The Realtime Data Computing Framework, which is build on golang, c and lua; Streaming Modules can be configed to define the processing routine; SDK for Client; Scalable Architecture and library for detail needs;"
+        name:"LibraT,PGM/Architecture/Dev,格灵深瞳[2017.12.1-NOW]",
+        describe:"基于3D摄像头，对视野范围内的人员进行识别，跟踪，跌倒，暴力，以及其他的行为分析。设备本身属于一体机，配有IOT平台，配置端以及监控端。",
+        more:"http://www.deepglint.com/haomu"
+    },
+    {
+        name:"DGNet,PGM/Architecture/Dev,格灵深瞳[2016.12.1-NOW]",
+        describe:"由大数据中心，机器学习平台，数据-模型自动化机器学习业务平台，标注平台，应用中心组成，打通数据-模型-应用的闭环"
+    },
+    {
+        name:"Backbone 全栈框架/Full-Stack Leader,格灵深瞳[2015.7.15-2016.11.31]",
+        describe:"面向业务平台级应用的中间层+前端框架。基于Vue.js,Golang,GRPC,Gulp等,可以实现模型驱动生成API+UI，组件库复用，自动化打包，发布，部署，集成CI等，可以支持X86，Arm等平台，目前是格灵深瞳所有应用的基础框架。具有积累性，目前同时支持10+平台级应用的生产，极其节省人力。"
+    },
+    {
+        name:"Monica 流式计算&业务框架/Full-Stack Leader,格灵深瞳[2015.7.15-2016.11.31]",
+        describe:"一套后端面向实时业务流、数据流的流式处理框架，主要基于golang,C++,lua,python构建; 通过配置文件可供自由加载流式组件，支持X86-64，Arm，Windows等平台和系统;简单易用，具有积累属性，目前应用于各大项目的业务处理，以及流式计算。"
     },{
         name:"ADU",
         describe:"The Auth Service for dealing with the relationship among devices, developer, project and users, based on OAuth2.0 "
